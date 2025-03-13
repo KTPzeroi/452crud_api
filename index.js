af1a75cecc9a5925298c8e24cbdbc0b52335bea5
KTPzeroi <zeroi7887@gmail.com>
2025-03-13 11:02:24 +0700

    second commit

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ app.delete('/users', (req, res) => {
     )
 })
 
-app.listen(process.env.PORT || 4000, () => {
-    console.log('CORS-enabled web server listening on port 4000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('CORS-enabled web server listening on port 3000')
 })
 
 // export the app for vercel serverless functions
